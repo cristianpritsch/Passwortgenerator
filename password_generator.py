816ee8f10640265375613dd59e4111561985ef2d
Cristian Pritsch <cristian.pritsch1@gmail.com>
2026-07-09 13:24:03 +0200

    Passwortgenerator hinzugefügt

---
+++ password_generator.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+print("=== Passwortgenerator ===")
+
+while True:
+    try:
+        length = int(input("Wie lang soll das Passwort sein? (mindestens 8 Zeichen): "))
+
+        if length < 8:
+            print("Bitte mindestens 8 Zeichen eingeben.\n")
+            continue
+
+        break
+
+    except ValueError:
+        print("Bitte eine gültige Zahl eingeben.\n")
+
+characters = (
+    string.ascii_letters +
+    string.digits +
+    string.punctuation
+)
+
+password = "".join(random.choice(characters) for _ in range(length))
+
+print("\nDein Passwort lautet:\n")
+print(password)
