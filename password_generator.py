f372fb9011360d0d6156b5d98c928bca043ab952
Cristian Pritsch <cristian.pritsch1@gmail.com>
2026-07-09 13:43:58 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,25 +4,29 @@ import string
 print("=== Passwortgenerator ===")
 
 while True:
-    try:
-        length = int(input("Wie lang soll das Passwort sein? (mindestens 8 Zeichen): "))
 
-        if length < 8:
-            print("Bitte mindestens 8 Zeichen eingeben.\n")
-            continue
+    while True:
+        try:
+            length = int(input("Wie lang soll das Passwort sein? (mindestens 8 Zeichen): "))
 
+            if length < 8:
+                print("Bitte mindestens 8 Zeichen eingeben.\n")
+                continue
+
+            break
+
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.\n")
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = "".join(random.choice(characters) for _ in range(length))
+
+    print("\nDein Passwort lautet:\n")
+    print(password)
+
+    erneut = input("\nMöchtest du ein weiteres Passwort erstellen? (j/n): ").lower()
+
+    if erneut != "j":
+        print("\nDanke für die Nutzung des Passwortgenerators!")
         break
-
-    except ValueError:
-        print("Bitte eine gültige Zahl eingeben.\n")
-
-characters = (
-    string.ascii_letters +
-    string.digits +
-    string.punctuation
-)
-
-password = "".join(random.choice(characters) for _ in range(length))
-
-print("\nDein Passwort lautet:\n")
-print(password)
